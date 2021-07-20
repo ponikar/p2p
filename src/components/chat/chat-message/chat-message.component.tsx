e947229777ef656bcfac29921c0f87bc0e942061
Darshan Ponikar <drsofficial3k@gmail.com>
2021-07-20 17:57:15 +0530

    Fixing max width bug in chat component

--- src/components/chat/chat-message/chat-message.component.tsx
+++ src/components/chat/chat-message/chat-message.component.tsx
@@ -15,18 +15,26 @@ export const ChatMessage: FC<ChatMessageType> = ({
   byUser = false,
 }) => {
   return (
-      <div
-        className={`flex w-full m-4 items-center relative ${byUser && "justify-end"}`}
-      >
-        {!byUser && (
-          <img
-            src="https://robohash.org/logo"
-            className="w-10 border h-10 object-contain rounded-full"
-          />
-        )}
+    <div
+      className={`flex w-full m-4 items-center relative ${
+        byUser && "justify-end"
+      }`}
+    >
+      {!byUser && (
+        <img
+          src="https://robohash.org/logo"
+          className="w-10 border h-10 object-contain rounded-full"
+        />
+      )}
 
-        <div className={`chat-message-area ${byUser && "bg-primary text-white"}`}>{text}</div>
-      {!isSameMessageBy && createdAt &&  <span className="text-xs absolute -bottom-6 text-highlight right-5"> {getMessageTime(createdAt)} </span>  }
-      </div>
+      <p className={`chat-message-area ${byUser && "bg-primary text-white"}`}>
+        {text}
+      </p>
+      {!isSameMessageBy && createdAt && (
+        <span className="text-xs absolute -bottom-6 text-highlight right-5">
+          {getMessageTime(createdAt)}
+        </span>
+      )}
+    </div>
   );
 };
